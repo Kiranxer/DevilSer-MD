09605713b3273d48ef57ab5a4fba88c877da0a3e
кιяαи ѕєя <105064751+Kiranxer@users.noreply.github.com>
2024-07-22 15:42:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@
 const toBool = (x) => x === 'true';
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'Devilser~AHs',
-    HANDLERS: process.env.HANDLERS || '.',//[.]
+    SESSION_ID: process.env.SESSION_ID || '',
+    HANDLERS: process.env.HANDLERS || '#',//[.]
     BOT_NAME: process.env.BOT_NAME || 'DevilSer_Md',
     OWNER_NAME: process.env.OWNER_NAME || '🙃',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '916282378078',
-    SUDO: process.env.SUDO || '916282378078',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '919539060020',
+    SUDO: process.env.SUDO || '919539060020',
     MODE: (process.env.MODE || 'public').trim(),
     STICKER_DATA: process.env.STICKER_DATA || '🎯inrl-𝙼𝙳;loki-x☘️',
 };
